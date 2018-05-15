4e7b95eb045f80fe42b30a8f99badf3648ddc572
elenasheloputova <esheloputova@jtconsulting.ru>
2018-05-15 23:56:27 +0300

    all10

--- src/test/java/PassTest.java
+++ src/test/java/PassTest.java
@@ -1,7 +1,9 @@
 import configuration.ConfigProperties;
+import models.User;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import pages.All10;
+import pages.LoginPage;
 import pages.Page;
 import ru.yandex.qatools.allure.annotations.Step;
 import ru.yandex.qatools.allure.annotations.Title;
@@ -19,12 +21,9 @@ public class PassTest {
     @Step("Pass test")
     @Test
     public void pastTest() {
-//        User user1 = new User(ConfigProperties.getTestProperty("anExistingLogin"), ConfigProperties.getTestProperty("correctPassword"));
-//        new LoginPage()
-//                .authorizationAnExistingUser(user1);
+        new All10().goTologin();
 
         new All10().goSertifications();
-
 
 
     }
